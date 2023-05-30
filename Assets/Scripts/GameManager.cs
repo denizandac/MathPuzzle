f4d76108345ae6a5957cb0f7c1e0248ee7eb9e9a
Deniz <denizandac33@gmail.com>
2023-05-30 11:29:17 +0300

    Added GameManager
    Game Manager implemented,
    Playing status added.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState gameState;
+    public enum GameState
+    {
+        Playing,
+        Paused,
+        GameOver
+    }
+    void Awake(){
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        gameState = GameState.Playing;
+    }
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameState == GameState.Playing)
+            {
+                gameState = GameState.Paused;
+                Time.timeScale = 0;
+            }
+            else if (gameState == GameState.Paused)
+            {
+                gameState = GameState.Playing;
+                Time.timeScale = 1f;
+            }
+        }
+    }
+    public void EndGame(){
+        gameState = GameState.GameOver;
+        Application.Quit();
+    }
+
+    
+}
